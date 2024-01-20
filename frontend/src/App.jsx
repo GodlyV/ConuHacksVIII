b7bd1e96be0d060f128686ce0e328e05646b00c8
moscatoN <nicolas.moscatobarbeau@gmail.com>
2024-01-20 17:51:00 -0500

    propmenu changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WardrobeCam from './components/WardrobeCam'
-
+import PropertiesMenu from './components/PropertiesMenu'
 function App() {
 
   return (
+    <>
+    <div class="PropertiesMenu">
+    <PropertiesMenu />
+    </div>
     <div>
     <WardrobeCam />
     </div>
+    </>
+    
   )
 }
 export default App
