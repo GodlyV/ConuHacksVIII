7d074e054a89e05a54f11cbe2925b7a8ac4c6933
guidote <gabrielle.guidote@gmail.com>
2024-01-20 13:52:05 -0500

    added coords lines

--- frontend/main.js
+++ frontend/main.js
@@ -7,19 +7,25 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// helper XYZ coords
+const coords = new THREE.AxesHelper(10);
+scene.add(coords);
+
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const material_cube = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const cube = new THREE.Mesh(geometry, material_cube);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 10;
 
 function animate() {
+    /*
     requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
+    */
+    cube.scale.set(5,5,5); // adjust the scale of the cube
     renderer.render(scene, camera);
 }
 
