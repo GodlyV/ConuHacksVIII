f6e8960247e3c39dfca632b456aa7ef465702a95
guidote <gabrielle.guidote@gmail.com>
2024-01-20 17:32:02 -0500

    added a wardrobe

--- frontend/src/components/WardrobeCam.jsx
+++ frontend/src/components/WardrobeCam.jsx
@@ -1,11 +1,20 @@
-    import React, { useEffect, useRef } from 'react';
     import * as THREE from 'three';
+    import React, { useEffect, useRef, useState } from 'react';
+
+    // DELETE LATER
+    import { MapControls } from 'three/addons/controls/MapControls.js';
+
     import { DragControls } from 'three/examples/jsm/controls/DragControls.js';
     import WardrobeDivider from '../classes/WardrobeDivider.jsx';
     import Door from '../classes/Door.jsx';
 
     const WardrobeCam = () => {
         const refContainer = useRef(null);
+        // Variables Width, Height, Depth
+        const [width, setWidth] = useState(3);
+        const [heigth, setHeigth] = useState(4);
+        const [depth, setDepth] = useState(1);
+        const [materialThickness, setMaterialThickness] = useState(0.25);
         
         const initScene = (refContainer) => {
             const scene = new THREE.Scene();
@@ -15,10 +24,50 @@
             const renderer = new THREE.WebGLRenderer();
             renderer.setSize(refContainer.current.clientWidth, refContainer.current.clientHeight);
             refContainer.current.appendChild(renderer.domElement);
+
+            // DELETE LATER
+            const controls = new MapControls(camera, renderer.domElement);
         
             return { scene, camera, renderer };
         };
 
+        const createWardrobe = (scene) => {
+            const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+            const material_cube = new THREE.MeshBasicMaterial({ color: 0xEA9999 });
+            const wardrobe_base = new THREE.Mesh(geometry, material_cube);
+            wardrobe_base.scale.set(width, materialThickness, depth); // Adjust the scale of the cube
+            wardrobe_base.position.set(0,0,0); // move the wardrobe so that the front of it is at z=0
+            scene.add(wardrobe_base);
+
+            const wardrobe_base_top = wardrobe_base.clone();
+            wardrobe_base_top.position.set(0, heigth - materialThickness/2, 0);
+            scene.add(wardrobe_base_top);
+
+            const material_cube0 = new THREE.MeshBasicMaterial({ color: 0xF9CB9C });
+            const wardrobe_back = new THREE.Mesh(geometry, material_cube0);
+            wardrobe_back.scale.set(width, materialThickness, heigth); // Adjust the scale of the cube
+            wardrobe_back.rotation.x = Math.PI / 2;
+            wardrobe_back.position.set(0, heigth/2 -materialThickness/2, -depth/2 + materialThickness/2); // move the wardrobe so that the front of it is at z=0
+            scene.add(wardrobe_back);
+
+
+            const material_cube1 = new THREE.MeshBasicMaterial({ color: 0xFFE599 });
+            const wardrobe_side = new THREE.Mesh(geometry, material_cube1);
+            wardrobe_side.scale.set(heigth, materialThickness, depth); // Adjust the scale of the cube
+
+            const wardrobe_side_r = wardrobe_side.clone();
+            wardrobe_side_r.rotation.z = Math.PI / 2;
+            wardrobe_side_r.position.set(width/2 - materialThickness/2, heigth/2 - materialThickness/2, 0); // move the wardrobe so that the front of it is at z=0
+            scene.add(wardrobe_side_r);
+
+            const wardrobe_side_l = wardrobe_side.clone();
+            wardrobe_side_l.rotation.z = Math.PI / 2;
+            wardrobe_side_l.position.set(-width/2 + materialThickness/2, heigth/2 - materialThickness/2, 0); // move the wardrobe so that the front of it is at z=0
+            scene.add(wardrobe_side_l);
+
+            return { wardrobe_base, wardrobe_base_top, wardrobe_back, wardrobe_side_r, wardrobe_side_l };
+        };
 
         const createWardrobeDivider = (scene) => {
             const divider = new WardrobeDivider();
@@ -41,7 +90,8 @@
         
             const coords = new THREE.AxesHelper(10);
             scene.add(coords);
-        
+            
+            const wardrobe = createWardrobe(scene);
             const door = createDoor(scene);
             const divider = createWardrobeDivider(scene);
 
@@ -68,13 +118,15 @@
                 dragControls.dispose();
                 renderer.dispose();
 
+                wardrobe.dispose();
+
 
                 // Dispose of the draggable mesh
                 scene.remove(door);
                 door.geometry.dispose();
                 door.material.dispose();
 
-                        // Remove other objects from the scene and dispose of their resources
+                // Remove other objects from the scene and dispose of their resources
                 // Example for a generic object added to the scene
                 scene.children.forEach(child => {
                     scene.remove(child);
